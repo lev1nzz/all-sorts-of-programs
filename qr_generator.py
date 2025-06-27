05cca694d17da289df5a9c72f5b985107d51f746
lev1nzz <xlev1n.xxxx@gmail.com>
2025-06-27 19:47:04 +0300

    добавил файл readme

--- qr_generator.py
+++ qr_generator.py
@@ -3,10 +3,10 @@ from segno import helpers
 
 username = os.getlogin()
 desktop = os.path.join(os.path.join(os.environ["USERPROFILE"]), "Desktop")
-save_path = os.path.join(desktop, "qr_WiFi.png")  # => "C:/Users/Admin/Desktop/my_qr.png"
+save_path = os.path.join(desktop, "qr_WiFi.png")  # => "C:/Users/user/Desktop/my_qr.png"
 
 
-Name = input('Enter Name WI-Fi:')
+Name = input('Enter Name WI-Fi:') 
 Passw = input('Enter a Password:')
 secu = input('Enter type Securiti (WPA/WPA2):')
 
